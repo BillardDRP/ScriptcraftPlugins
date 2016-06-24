b57aa89e30237c23e693a26f9c9fefe15e9a5f26
Diego Eann <deann21@punahou.edu>
2016-06-23 16:30:45 -1000

    Added lighthouse and gate projects

--- HouseEnt.js
+++ HouseEnt.js
@@ -1,12 +1,15 @@
 'use strict'
 
 var Drone = require('drone'),
-    blocks = require('blocks');
+    blocks = require('blocks')
+    sounds = require('sounds');
 
 function HouseEnt() {
   this.box0("5",8,4,8)
       .box("1",1,1,1)
       .up(4)
-      .prisim("5",8,4);
+      .prisim("5",8,4)
+      sounds.successful_hit();
+  // sounds.play(BLOCK_WOOD_PRESSUREPLATE_CLICK_ON,self);
     };
 Drone.extend(HouseEnt)
