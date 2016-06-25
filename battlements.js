956f46957788c394e422d406fe3912871d2b8d81
Diego Eann <deann21@punahou.edu>
2016-06-24 16:31:17 -1000

    did some stuff

---
+++ battlements.js
@@ -0,0 +1,22 @@
+'use strict'
+    // fetch databases
+var Drone = require('drone'),
+    blocks = require('blocks')
+    sounds = require('sounds');
+// function start
+    function EntBattlements(amount) {
+      // customization start
+        if (typeof amount=='undefined'){
+          amount=1
+        }
+      // End customization, start function
+        while(amount!=1){
+          for(){
+
+          }
+
+    }
+
+
+
+ Drone.extend(EntBattlements)
